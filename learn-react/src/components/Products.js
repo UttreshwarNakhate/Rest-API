74063ab860737f90972b59d470c1dc96d458ff75
un-the-dev <104813812+UttreshwarNakhate@users.noreply.github.com>
2024-08-13 18:36:52 +0530

    Added proxy url to fetch endpoints result

--- learn-react/src/components/Products.js
+++ learn-react/src/components/Products.js
@@ -10,7 +10,7 @@ const Products = () => {
   useEffect(() => {
     console.log("Function mounted...")
 
-    fetch('/server/api/product')
+    fetch('/api/products')
       .then(response =>{ 
         console.log("response: ", response)
         return response.json()
@@ -21,13 +21,15 @@ const Products = () => {
       })
   }, [])
 
+  console.log("products: ", products)
+
   return (
     <>
       <div className="container mx-auto pb-24">
         <h1 className="text-lg font-bold my-8">Products</h1>
         <div className="grid grid-cols-5 my-8 gap-24">
           {
-            productsArray.map(product => <Product key={product._id} product={product}/>)
+            products.map(product => <Product key={product._id} product={product}/>)
           }
         </div>
       </div> 
