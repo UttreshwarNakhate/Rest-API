7cce5a7e0223c810d03f7c8a754dd17dfbb03e02
un-the-dev <104813812+UttreshwarNakhate@users.noreply.github.com>
2024-08-22 14:34:30 +0530

    About us updated

--- learn-react/src/pages/About.js
+++ learn-react/src/pages/About.js
@@ -1,24 +1,28 @@
-
-import Card from 'react-bootstrap/Card';
-
 const About = () => {
   return (
+    <div className="flex mx-auto bg-orange-100 rounded-xl justify-center shadow-xl item-center w-1/2 border-collapse">
+      <div className="card-body">
+        <h2 className="card-title">About US!</h2>
+        <p className="text-md">
+          Welcome to Pizza_Hub, your go-to destination for delicious,
+          hand-crafted pizzas delivered straight to your door. We pride
+          ourselves on using only the freshest ingredients and offering a wide
+          range of flavors to satisfy every pizza lover's cravings.
+        </p>
+        <strong>
+          At Pizza_Hub, we’re all about convenience, quality, and community.
+          Whether you're ordering for a quick lunch or a cozy night in, we've
+          made it easy to enjoy your favorite pizza with just a few taps.
+        </strong>
+        <div className="flex justify-center">
+          <p className="text-sm">
+            Thank you for choosing Pizza_Hub. We’re here to serve you the best
+            pizza experience, every time.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-    <Card style={{ width: '18rem' }}>
-    <Card.Body>
-      <Card.Title>About US!</Card.Title>
-      {/* <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle> */}
-      <Card.Text>
-      Welcome to Pizza_Hub, your go-to destination for delicious, hand-crafted pizzas delivered straight to your door. We pride ourselves on using only the freshest ingredients and offering a wide range of flavors to satisfy every pizza lover's cravings.
-
-At Pizza_Hub, we’re all about convenience, quality, and community. Whether you're ordering for a quick lunch or a cozy night in, we've made it easy to enjoy your favorite pizza with just a few taps.
-      </Card.Text>
-      <Card.Link href="#">Thank you for choosing Pizza_Hub. We’re here to serve you the best pizza experience, every time.</Card.Link>
-      <Card.Link href="#">Another Link</Card.Link>
-    </Card.Body>
-  </Card>
-
-  )
-}
-
-export default About
+export default About;
