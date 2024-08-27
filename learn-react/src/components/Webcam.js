f334870757bce9c588699cec16d8452854c882ed
un-the-dev <104813812+UttreshwarNakhate@users.noreply.github.com>
2024-08-27 18:03:30 +0530

    Added images

--- learn-react/src/components/Webcam.js
+++ learn-react/src/components/Webcam.js
@@ -49,13 +49,6 @@ const Camera = ({ setCapturedImage }) => {
         >
           Capture
         </button>
-        {/* <button
-          type="button"
-          className="rounded-md bg-orange-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          onClick={() => setUrl(null)}
-        >
-          Refresh
-        </button> */}
       </div>
 
       {url && (
