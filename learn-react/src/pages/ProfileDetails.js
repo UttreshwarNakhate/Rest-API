4ff91641114f4cab4e9a55f14d17cc77765ba420
un-the-dev <104813812+UttreshwarNakhate@users.noreply.github.com>
2024-08-28 12:05:59 +0530

    Added backedn path in the frontend

--- learn-react/src/pages/ProfileDetails.js
+++ learn-react/src/pages/ProfileDetails.js
@@ -36,7 +36,7 @@ function ProfileDetails() {
           >
             <div className="flex-none w-full relative h-48">
               <img
-                src={`http://192.168.5.20:5000/${user.image?.path}`}
+                src={`https://rest-backend-nu.vercel.app/${user.image?.path}`}
                 alt=""
                 className="absolute inset-0 w-full h-full object-cover"
                 loading="lazy"
