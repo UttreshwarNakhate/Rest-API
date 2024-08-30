0c710ea8f6af327a451186db6a56a92ecdb8fed8
un-the-dev <104813812+UttreshwarNakhate@users.noreply.github.com>
2024-08-30 14:41:45 +0530

    Update ProfileDetails.js

--- learn-react/src/pages/ProfileDetails.js
+++ learn-react/src/pages/ProfileDetails.js
@@ -36,7 +36,7 @@ function ProfileDetails() {
           >
             <div className="flex-none w-full relative h-48">
               <img
-                src={`http://localhost:5000/${user.image?.path}`}
+                src={`https://pizza-backend.techorizon.in/${user.image?.path}`}
                 alt=""
                 className="absolute inset-0 w-full h-full object-cover"
                 loading="lazy"
